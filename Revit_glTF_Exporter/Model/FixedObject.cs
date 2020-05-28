d53b399d66ad5a96947a763b1a09c249701a75b8
Francisco Possetto <franciscopossetto@hotmail.com>
2020-05-28 17:56:47 -0300

    -add files to create objects

---
+++ Revit_glTF_Exporter/Model/FixedObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_glTF_Exporter.Model
+{
+    public class FixedObject
+    {
+
+    }
+}
---
+++ Revit_glTF_Exporter/Model/MovableObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_glTF_Exporter.Model
+{
+    class MovableObject
+    {
+    }
+}
