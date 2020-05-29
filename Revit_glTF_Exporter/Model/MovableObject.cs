814c99f4965393595c0380863f337e06555474b3
Francisco Possetto <franciscopossetto@hotmail.com>
2020-05-29 10:22:25 -0300

    -add movable objects properties

--- Revit_glTF_Exporter/Model/MovableObject.cs
+++ Revit_glTF_Exporter/Model/MovableObject.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.DB;
 
 namespace Revit_glTF_Exporter.Model
 {
     class MovableObject
     {
+        Category Category { get; set; }
+        FamilySymbol FamilySymbol { get; set; }
+        string ElementName { get; set; }
+        ElementId ElementId { get; set; }
+        XYZ Location { get; set; }
     }
 }
---
+++ Revit_glTF_Exporter/Model/MovableObjects.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace Revit_glTF_Exporter.Model
+{
+    public class MovableObjects
+    {
+        public List<MovableObjects> MovableObjectsList { get; set; }
+
+        public int Count
+        {
+            get { return Count; }
+            set
+            {
+                this.Count = MovableObjectsList.Count();
+            }
+        }
+
+        public Category Category
+        {
+            get { return Category; }
+            set
+            {
+                this.Category = MovableObjectsList.FirstOrDefault().Category;
+            }
+        }
+
+
+
+
+
+
+    }
+}
